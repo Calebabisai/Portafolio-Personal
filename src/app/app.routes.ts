6ac081294a84eeda930844dbe4b4a21b421dc5c4
Calebabisai <gelndcaleb@gmail.com>
2025-11-11 15:14:28 -0600

    Primer commit del Portafolio, componentes creados, y diseno con HTML y CSS

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Courses } from './pages/courses/courses';
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
+import { Skills} from './pages/skills/skills';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'about', component: About},
+    {path: 'skills', component: Skills},
+    {path: 'projects', component: Projects},
+    {path: 'courses', component: Courses},
+    {path: 'contact', component: Contact},
+    {path: '**', redirectTo: ''},
+];
