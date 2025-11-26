b6db8445e51e305e22e44b22e4c459913eb81adf
Calebabisai <gelndcaleb@gmail.com>
2025-11-26 00:22:01 -0600

    Modificacion: Se ha agregado getInitialLanguage, para detectar el idioma automaticamente

--- src/app/services/language.service.ts
+++ src/app/services/language.service.ts
@@ -9,7 +9,8 @@ export class LanguageService {
   /*Signal que almacena el idioma actual
   Por defecto es espanol ('es')
   */
-  readonly currentLanguage = signal<Language>('es')
+  readonly currentLanguage = signal<Language>(this.getInitialLanguage());
+  
 
   /**
    * Objeto de tradcucciones
@@ -21,6 +22,28 @@ export class LanguageService {
     const savedLanguage = localStorage.getItem('language') as Language;
     if (savedLanguage && (savedLanguage === 'es' || savedLanguage === 'en')) {
       this.currentLanguage.set(savedLanguage);
+    }
+  }
+
+  /**
+   * Detecta el idioma del navegador
+   * Si el navegador esta en ingles -> muestra en ingles
+   * Si no -> muestra en español
+   */
+
+  private getInitialLanguage(): Language {
+    // Primero se verifica si el usuario ya escogio el idioma
+    const savedLanguage = localStorage.getItem('language') as Language;
+    if(savedLanguage && (savedLanguage === 'es' || savedLanguage === 'en')) {
+      return savedLanguage;
+    }
+
+    // Si no, se detecta el idioma del navegador
+    const browserLang = navigator.language.slice(0, 2).toLowerCase();
+    if (browserLang === 'en') {
+      return 'en';
+    } else {
+      return 'es';
     }
   }
 
