071f958aa9ce0143d68a0e1342ec2ab7a7cf396f
Calebabisai <gelndcaleb@gmail.com>
2025-11-29 17:49:47 -0600

    feat: Se ha regresado a gemini2.5 flash

--- src/app/services/gemini.service.ts
+++ src/app/services/gemini.service.ts
@@ -38,7 +38,7 @@ export class GeminiService {
     /**
      * Selecciona el modelo generativo a utilizar (Gemini 2.5 Flash)
      */
-    this.model = this.genAI.getGenerativeModel({model: 'gemini-pro'});
+    this.model = this.genAI.getGenerativeModel({model: 'gemini-2.5-flash'});
     /** Inicializa el chat con el modelo seleccionado */
     this.initializeChat();
   }
